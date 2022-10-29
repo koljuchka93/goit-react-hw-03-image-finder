f42ba2096ecbb1c323b5ce1474fdf586787f77f6
Kateryna <koljuchka93@gmail.com>
2022-10-30 00:35:39 +0300

    add base

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,1 @@
+<button>Load more</button>
---
+++ src/components/Styles.styled.jsx
@@ -0,0 +1,12 @@
+/* 
+Створи компоненти 
+
+<Searchbar>
+<ImageGallery>
+<ImageGalleryItem>
+<Loader>
+<Button> 
+<Modal> 
+
+Готові стилі компонентів можна взяти у файлі styles.css і підправити під себе, якщо потрібно.
+*/
